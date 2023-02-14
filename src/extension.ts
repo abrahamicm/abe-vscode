d66be9e48d2f608c85634bd3df7199c24810a361
abrahan latitut <abrahamicm2@gmail.com>
2023-02-14 03:22:00 -0400

    .

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,9 @@ export function activate(context: vscode.ExtensionContext) {
     const editor = vscode.window.activeTextEditor;
     if (!editor) return;
     const selection = editor.selection;
-    const text = editor.document.getText(selection).replace(/\n/g, "', '");
+    const text = editor.document.getText(selection)
+      .replace(/[\r\n]+(\s*)/g, "', '") // Eliminar cualquier espacio en blanco adicional después del último carácter de la línea
+      .trim(); // Eliminar cualquier espacio en blanco adicional al principio y al final del texto
     editor.edit(editBuilder => {
       editBuilder.replace(selection, `\$array = ['${text}'];`);
     });
